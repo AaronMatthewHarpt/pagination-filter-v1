b93f1b5b9775f37372f64c8e499c11eb5f0b143b
aaron.harpt <aaron.harpt@gmail.com>
2016-04-22 15:02:36 -0500

    changed element that is getting appended

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,2 @@
 // appends search to page
-$("h2").append('<div class="student-search"><input placeholder="Searching for students..."><button>Search</button></div>');
+$(".page-header").append('<div class="student-search"><input placeholder="Searching for students..."><button>Search</button></div>');
