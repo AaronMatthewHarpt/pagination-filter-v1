aeef49c7b603457abaf5b3845ae94a59f5f562cb
aaron.harpt <aaron.harpt@gmail.com>
2016-04-22 14:56:07 -0500

    worked on appending search

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
-$("body").prepend('<label for="search">Search</label><input type="text" id="search" name="search"/>');
+// appends search to page
+$("h2").append('<div class="student-search"><input placeholder="Searching for students..."><button>Search</button></div>');
