c6bf5b321299a902ca4b8e7f563cc46557a15076
aaron.harpt <aaron.harpt@gmail.com>
2016-04-26 15:57:21 -0500

    worked on project

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 /*
 Pagination Content Filter:
 */
+// Global variables
+var studentsPerPage = 10;
 
 // appends search to page
 $(".page-header").append('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
 
 
 // adds pagination links
+for (var numberLinks = ; i > 10; i += 1) {
+
+}
