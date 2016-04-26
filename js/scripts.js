eb5a89fc55faca9651dfae2401959f514e514932
aaron.harpt <aaron.harpt@gmail.com>
2016-04-26 15:53:52 -0500

    added // adds pagination links comment

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,6 @@ Pagination Content Filter:
 
 // appends search to page
 $(".page-header").append('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
+
+
+// adds pagination links
