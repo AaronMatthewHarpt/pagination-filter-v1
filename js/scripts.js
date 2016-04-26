7867cf117048f8ae14d6bd9a987f18da8a7ad5e3
aaron.harpt <aaron.harpt@gmail.com>
2016-04-26 15:52:24 -0500

    added /* Pagination Content Filter:*/ comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
+/*
+Pagination Content Filter:
+*/
+
 // appends search to page
 $(".page-header").append('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
