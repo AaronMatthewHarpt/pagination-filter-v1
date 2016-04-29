f3ec368ed4d1bb7991b78028d6146e4b232da9e5
aaron.harpt <aaron.harpt@gmail.com>
2016-04-29 08:56:39 -0500

    worked on pagination

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ Pagination Content Filter:
 */
 // Global variables
 var studentsPerPage = 10;
-var $totalStudents = $(".student-list").find(".student-item");
+var totalStudents = $(".student-list").find(".student-item");
 var numofLinks = studentsPerPage / 10;
 
 
-console.log($totalStudents);
+console.log(totalStudents);
 
 
 // appends search to page
@@ -19,14 +19,15 @@ $(".page-header").append('<div class="student-search"><input placeholder="Search
 // hiding all students when the page first loads
 $(".student-item").hide();
 
-// showing the first 10 students
-var $studentsPerPage = $(".student-item:lt(10)").show();
-
 // appending the pagination div and child ul
 $(".page").append('<div class="pagination"><ul></ul></div>');
 
+// showing the first 10 students
+var $studentsPerPage = $(".student-item:lt(10)").show();
+
+
 // adding pagination links
 
-if ($totalStudents / 10 > 0) {
+if (totalStudents / 10 > 0) {
   $(".pagination ul").append('<li><a href="#">2</a></li>');
 }
